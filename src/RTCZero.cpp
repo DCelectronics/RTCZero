682ddf3f103079da13a5ac6a26b42e1b75e34231
EricB <eric@baril.com>
2018-04-18 15:25:51 -0400

    Added SAMD51 support
    Testing Done using ATMEL XPLAINED PRO   E54.
    Uses the same Specs as D51.
    simply add : #define __SAMD51__ to make it "arduino" ready.
    
    TODO:
    - Alarm Date not tested 
    - standbyMode not tested 
    - make a testExample.ino for complete unit testing

--- src/RTCZero.cpp
+++ src/RTCZero.cpp
@@ -1,17 +1,14 @@
 /*
   RTC library for Arduino Zero.
   Copyright (c) 2015 Arduino LLC. All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
@@ -43,7 +40,10 @@ void RTCZero::begin(bool resetTime)
 {
   uint16_t tmp_reg = 0;
   
+#if !defined(__SAMD51__)
   PM->APBAMASK.reg |= PM_APBAMASK_RTC; // turn on digital interface clock
+#endif
+
   config32kOSC();
 
   // If the RTC is in clock mode and the reset was
@@ -52,65 +52,103 @@ void RTCZero::begin(bool resetTime)
   bool validTime = false;
   RTC_MODE2_CLOCK_Type oldTime;
 
-  if ((!resetTime) && (PM->RCAUSE.reg & (PM_RCAUSE_SYST | PM_RCAUSE_WDT | PM_RCAUSE_EXT))) {
-    if (RTC->MODE2.CTRL.reg & RTC_MODE2_CTRL_MODE_CLOCK) {
-      validTime = true;
-      oldTime.reg = RTC->MODE2.CLOCK.reg;
-    }
-  }
+#if defined(__SAMD51__)
+	/*
+	if ((!resetTime) && (PM->RCAUSE.reg & (PM_RCAUSE_SYST | PM_RCAUSE_WDT | PM_RCAUSE_EXT))) {
+		if (RTC->MODE2.CTRL.reg & RTC_MODE2_CTRL_MODE_CLOCK) {
+			validTime = true;
+			oldTime.reg = RTC->MODE2.CLOCK.reg;
+		}
+	}
+	*/
+	configureClock();
+	RTCdisable();
+	RTCreset();
+	
+	tmp_reg |= RTC_MODE2_CTRLA_MODE_CLOCK; // set clock operating mode
+	tmp_reg |= RTC_MODE2_CTRLA_PRESCALER_DIV1024; // set prescaler to 1024 for MODE2
+	tmp_reg &= ~RTC_MODE2_CTRLA_MATCHCLR; // disable clear on match
+	tmp_reg &= ~RTC_MODE2_CTRLA_CLKREP; // 24h time representation //According to the datasheet RTC_MODE2_CTRL_CLKREP = 0 for 24h
+	RTC->MODE2.CTRLA.reg = tmp_reg;
+	while (RTCisSyncing());
+	
+	NVIC_EnableIRQ(RTC_IRQn); // enable RTC interrupt
+	NVIC_SetPriority(RTC_IRQn, 0x00);
 
-  // Setup clock GCLK2 with OSC32K divided by 32
-  configureClock();
+	RTC->MODE2.INTENSET.reg					|= RTC_MODE2_INTENSET_ALARM0; // enable alarm interrupt
+	RTC->MODE2.Mode2Alarm[0].MASK.bit.SEL    = MATCH_OFF; // default alarm match is off (disabled)
+	while (RTCisSyncing());
 
-  RTCdisable();
+	RTCenable();
+	RTCresetRemove();
+	
+#else	/* samd21 original support */
 
-  RTCreset();
+	if ((!resetTime) && (PM->RCAUSE.reg & (PM_RCAUSE_SYST | PM_RCAUSE_WDT | PM_RCAUSE_EXT))) {
+		if (RTC->MODE2.CTRL.reg & RTC_MODE2_CTRL_MODE_CLOCK) {
+			validTime = true;
+			oldTime.reg = RTC->MODE2.CLOCK.reg;
+		}
+	}
 
-  tmp_reg |= RTC_MODE2_CTRL_MODE_CLOCK; // set clock operating mode
-  tmp_reg |= RTC_MODE2_CTRL_PRESCALER_DIV1024; // set prescaler to 1024 for MODE2
-  tmp_reg &= ~RTC_MODE2_CTRL_MATCHCLR; // disable clear on match
+	// Setup clock GCLK2 with OSC32K divided by 32
+	configureClock();
+	RTCdisable();
+	RTCreset();
+
+	tmp_reg |= RTC_MODE2_CTRL_MODE_CLOCK; // set clock operating mode
+	tmp_reg |= RTC_MODE2_CTRL_PRESCALER_DIV1024; // set prescaler to 1024 for MODE2
+	tmp_reg &= ~RTC_MODE2_CTRL_MATCHCLR; // disable clear on match
   
-  //According to the datasheet RTC_MODE2_CTRL_CLKREP = 0 for 24h
-  tmp_reg &= ~RTC_MODE2_CTRL_CLKREP; // 24h time representation
+	//According to the datasheet RTC_MODE2_CTRL_CLKREP = 0 for 24h
+	tmp_reg &= ~RTC_MODE2_CTRL_CLKREP; // 24h time representation
 
-  RTC->MODE2.READREQ.reg &= ~RTC_READREQ_RCONT; // disable continuously mode
+	RTC->MODE2.READREQ.reg &= ~RTC_READREQ_RCONT; // disable continuously mode
 
-  RTC->MODE2.CTRL.reg = tmp_reg;
-  while (RTCisSyncing())
-    ;
+	RTC->MODE2.CTRL.reg = tmp_reg;
+	while (RTCisSyncing())
+	;
 
-  NVIC_EnableIRQ(RTC_IRQn); // enable RTC interrupt 
-  NVIC_SetPriority(RTC_IRQn, 0x00);
+	NVIC_EnableIRQ(RTC_IRQn); // enable RTC interrupt 
+	NVIC_SetPriority(RTC_IRQn, 0x00);
 
-  RTC->MODE2.INTENSET.reg |= RTC_MODE2_INTENSET_ALARM0; // enable alarm interrupt
-  RTC->MODE2.Mode2Alarm[0].MASK.bit.SEL = MATCH_OFF; // default alarm match is off (disabled)
+	RTC->MODE2.INTENSET.reg |= RTC_MODE2_INTENSET_ALARM0; // enable alarm interrupt
+	RTC->MODE2.Mode2Alarm[0].MASK.bit.SEL = MATCH_OFF; // default alarm match is off (disabled)
   
-  while (RTCisSyncing())
-    ;
+	while (RTCisSyncing())
+	;
 
-  RTCenable();
-  RTCresetRemove();
+	RTCenable();
+	RTCresetRemove();
 
-  // If desired and valid, restore the time value, else use first valid time value
-  if ((!resetTime) && (validTime) && (oldTime.reg != 0L)) {
-    RTC->MODE2.CLOCK.reg = oldTime.reg;
-  }
-  else {
-    RTC->MODE2.CLOCK.reg = RTC_MODE2_CLOCK_YEAR(DEFAULT_YEAR - 2000) | RTC_MODE2_CLOCK_MONTH(DEFAULT_MONTH) 
-        | RTC_MODE2_CLOCK_DAY(DEFAULT_DAY) | RTC_MODE2_CLOCK_HOUR(DEFAULT_HOUR) 
-        | RTC_MODE2_CLOCK_MINUTE(DEFAULT_MINUTE) | RTC_MODE2_CLOCK_SECOND(DEFAULT_SECOND);
-  }
-  while (RTCisSyncing())
-    ;
+	// If desired and valid, restore the time value, else use first valid time value
+	if ((!resetTime) && (validTime) && (oldTime.reg != 0L)) {
+		RTC->MODE2.CLOCK.reg = oldTime.reg;
+	}
+	else {
+	RTC->MODE2.CLOCK.reg = RTC_MODE2_CLOCK_YEAR(DEFAULT_YEAR - 2000) | 
+							RTC_MODE2_CLOCK_MONTH(DEFAULT_MONTH) | 
+							RTC_MODE2_CLOCK_DAY(DEFAULT_DAY) | 
+							RTC_MODE2_CLOCK_HOUR(DEFAULT_HOUR) | 
+							RTC_MODE2_CLOCK_MINUTE(DEFAULT_MINUTE) | 
+							RTC_MODE2_CLOCK_SECOND(DEFAULT_SECOND);
+	}
+	while (RTCisSyncing())
+	;
+	
 
-  _configured = true;
+#endif	
+
+	_configured = true;
 }
+
 
 void RTC_Handler(void)
 {
   if (RTC_callBack != NULL) {
     RTC_callBack();
   }
+  
 
   RTC->MODE2.INTFLAG.reg = RTC_MODE2_INTFLAG_ALARM0; // must clear flag at end
 }
@@ -441,6 +479,10 @@ void RTCZero::setY2kEpoch(uint32_t ts)
 
 /* Attach peripheral clock to 32k oscillator */
 void RTCZero::configureClock() {
+#if defined(__SAMD51__)
+	/* No GCLK required for D51_E5x RTC module */
+#else
+
   GCLK->GENDIV.reg = GCLK_GENDIV_ID(2)|GCLK_GENDIV_DIV(4);
   while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY)
     ;
@@ -450,6 +492,7 @@ void RTCZero::configureClock() {
   GCLK->CLKCTRL.reg = (uint32_t)((GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK2 | (RTC_GCLK_ID << GCLK_CLKCTRL_ID_Pos)));
   while (GCLK->STATUS.bit.SYNCBUSY)
     ;
+#endif
 }
 
 /*
@@ -459,53 +502,103 @@ void RTCZero::configureClock() {
 /* Configure the 32768Hz Oscillator */
 void RTCZero::config32kOSC() 
 {
-  SYSCTRL->XOSC32K.reg = SYSCTRL_XOSC32K_ONDEMAND |
+	
+#if defined(__SAMD51__)
+	
+	/* Selecting the XOSC32k  --> 1k output as RTCclock src*/
+	OSC32KCTRL->RTCCTRL.reg = OSC32KCTRL_RTCCTRL_RTCSEL_XOSC1K;
+	while( (OSC32KCTRL->STATUS.reg & OSC32KCTRL_STATUS_XOSC32KRDY) == 0 );
+
+	/* XOSC32K activated in startup.c but activating again in case USER-code stopped it */
+	/* Enabling the "1k" output */
+	
+	OSC32KCTRL->XOSC32K.reg =   OSC32KCTRL_XOSC32K_XTALEN|
+								OSC32KCTRL_XOSC32K_ENABLE |
+								OSC32KCTRL_XOSC32K_EN32K |
+								OSC32KCTRL_XOSC32K_EN1K |
+								OSC32KCTRL_XOSC32K_CGM_XT |
+								OSC32KCTRL_XOSC32K_RUNSTDBY |
+								OSC32KCTRL_XOSC32K_ONDEMAND |
+								OSC32KCTRL_XOSC32K_STARTUP(3);
+								
+	
+#else
+	SYSCTRL->XOSC32K.reg = SYSCTRL_XOSC32K_ONDEMAND |
                          SYSCTRL_XOSC32K_RUNSTDBY |
                          SYSCTRL_XOSC32K_EN32K |
                          SYSCTRL_XOSC32K_XTALEN |
                          SYSCTRL_XOSC32K_STARTUP(6) |
                          SYSCTRL_XOSC32K_ENABLE;
+#endif			
 }
 
 /* Synchronise the CLOCK register for reading*/
 inline void RTCZero::RTCreadRequest() {
-  if (_configured) {
-    RTC->MODE2.READREQ.reg = RTC_READREQ_RREQ;
-    while (RTCisSyncing())
-      ;
-  }
+	if (_configured) {
+
+		#if defined(__SAMD51__)
+			RTC->MODE2.CTRLA.bit.CLOCKSYNC = 1;
+		#else	  
+			RTC->MODE2.READREQ.reg = RTC_READREQ_RREQ;
+		#endif
+
+		while (RTCisSyncing());
+	}
 }
 
 /* Wait for sync in write operations */
 inline bool RTCZero::RTCisSyncing()
 {
-  return (RTC->MODE2.STATUS.bit.SYNCBUSY);
+#if defined(__SAMD51__)
+	return (RTC->MODE2.SYNCBUSY.reg );
+#else
+	return (RTC->MODE2.STATUS.bit.SYNCBUSY);
+#endif	
+  
 }
 
 void RTCZero::RTCdisable()
 {
-  RTC->MODE2.CTRL.reg &= ~RTC_MODE2_CTRL_ENABLE; // disable RTC
-  while (RTCisSyncing())
-    ;
+	#if defined(__SAMD51__)
+
+	#else
+		RTC->MODE2.CTRL.reg &= ~RTC_MODE2_CTRL_ENABLE; // disable RTC
+	#endif
+	
+	while (RTCisSyncing());
 }
 
 void RTCZero::RTCenable()
 {
-  RTC->MODE2.CTRL.reg |= RTC_MODE2_CTRL_ENABLE; // enable RTC
-  while (RTCisSyncing())
-    ;
+	#if defined(__SAMD51__)
+		RTC->MODE2.CTRLA.reg |= RTC_MODE2_CTRLA_ENABLE; // enable RTC
+	#else
+		RTC->MODE2.CTRL.reg |= RTC_MODE2_CTRL_ENABLE; // enable RTC
+	#endif
+	
+	while (RTCisSyncing());
 }
 
 void RTCZero::RTCreset()
 {
-  RTC->MODE2.CTRL.reg |= RTC_MODE2_CTRL_SWRST; // software reset
-  while (RTCisSyncing())
-    ;
+	#if defined(__SAMD51__)
+	
+	#else
+	RTC->MODE2.CTRL.reg |= RTC_MODE2_CTRL_SWRST; // software reset
+	#endif
+	
+	while (RTCisSyncing());
+	
 }
 
 void RTCZero::RTCresetRemove()
 {
-  RTC->MODE2.CTRL.reg &= ~RTC_MODE2_CTRL_SWRST; // software reset remove
+	#if defined(__SAMD51__)
+		RTC->MODE2.CTRLA.reg &= ~RTC_MODE2_CTRLA_SWRST; // software reset remove
+	#else
+		RTC->MODE2.CTRL.reg &= ~RTC_MODE2_CTRL_SWRST; // software reset remove
+	#endif
+		
   while (RTCisSyncing())
     ;
 }
